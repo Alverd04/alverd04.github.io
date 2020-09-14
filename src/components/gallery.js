a5967b5bdd235e162dffdac9c2a1e9ffd19ff0f6
Albert Font <albert.font.fernandez@gmail.com>
2020-09-14 17:48:39 +0200

    all ok version

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -5,8 +5,8 @@ const gallery = () => {
     return(
         <div className='gallery'>
             <div className='row'>
-                <img src="./img/one.jpg" alt=""/>
-                <img src="./img/two.jpg" alt=""/>
+                <img src="https://paranoidandroid.co/assets/wallpapers/2018/submerged_desktop_thumb.jpg" alt=""/>
+                <img src="https://paranoidandroid.co/assets/wallpapers/2018/submerged_desktop_thumb.jpg" alt=""/>
                 <img src="https://paranoidandroid.co/assets/wallpapers/2018/submerged_desktop_thumb.jpg" alt=""/>
             </div>
             <div className='row'>
