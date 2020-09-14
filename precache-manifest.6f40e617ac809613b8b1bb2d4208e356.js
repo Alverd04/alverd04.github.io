7732bec4b3e5bed4468431b678f3eb828a71b193
unknown <albert.fontf@mataro.epiaedu.cat>
2020-09-14 11:38:35 +0200

    Updates

---
+++ precache-manifest.6f40e617ac809613b8b1bb2d4208e356.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9cfb81bd3765e79996cc442242cc08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7e05b79d598e2b09f51",
+    "url": "/static/css/main.30d36642.chunk.css"
+  },
+  {
+    "revision": "6207b1593c745030c5ad",
+    "url": "/static/js/2.87cbabeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.87cbabeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7e05b79d598e2b09f51",
+    "url": "/static/js/main.21ae304f.chunk.js"
+  },
+  {
+    "revision": "9ea7d63e00f86eba6c83",
+    "url": "/static/js/runtime-main.abd30af2.js"
+  },
+  {
+    "revision": "5ae8a204b7aa90a997b04d346655b926",
+    "url": "/static/media/welcome.5ae8a204.jpg"
+  }
+]);
