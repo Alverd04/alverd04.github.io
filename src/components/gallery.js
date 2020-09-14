8c67d670f6c3adff5e1de145dec816b00c8773e4
Albert Font <albert.font.fernandez@gmail.com>
2020-09-14 17:20:21 +0200

    updt

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -7,17 +7,17 @@ const gallery = () => {
             <div className='row'>
                 <img src="./img/one.jpg" alt=""/>
                 <img src="./img/two.jpg" alt=""/>
-                <img src="https://paranoidandroid.co/assets/wallpapers/2018/submerged_desktop_thumb.jpg" alt=""/>
+                <img src="./img/three.jpg" alt=""/>
             </div>
             <div className='row'>
-                <img src="https://paranoidandroid.co/assets/wallpapers/2018/submerged_desktop_thumb.jpg" alt=""/>
-                <img src="https://paranoidandroid.co/assets/wallpapers/2018/submerged_desktop_thumb.jpg" alt=""/>
-                <img src="https://paranoidandroid.co/assets/wallpapers/2018/submerged_desktop_thumb.jpg" alt=""/>
+                <img src="./img/four.jpg" alt=""/>
+                <img src="./img/five.jpg" alt=""/>
+                <img src="./img/six.jpg" alt=""/>
             </div>
             <div className='row'>
-                <img src="https://paranoidandroid.co/assets/wallpapers/2018/submerged_desktop_thumb.jpg" alt=""/>
-                <img src="https://paranoidandroid.co/assets/wallpapers/2018/submerged_desktop_thumb.jpg" alt=""/>
-                <img src="https://paranoidandroid.co/assets/wallpapers/2018/submerged_desktop_thumb.jpg" alt=""/>
+                <img src="./img/seven.jpg" alt=""/>
+                <img src="./img/eight.jpg" alt=""/>
+                <img src="./img/nine.jpg" alt=""/>
             </div>
         </div>
     )
